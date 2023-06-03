c40cd1a0fbe47c9ae00c9b6319d232db8aadf476
Luna <git@l4.pm>
2023-06-03 18:47:49 -0300

    add instruction to generated seed prompt

--- prepare/self_instruct.py
+++ prepare/self_instruct.py
@@ -1,6 +1,30 @@
 import sys
 import csv
 
+# Vicuna-13b-cocktail
+# https://huggingface.co/reeducator/vicuna-13b-cocktail
+CHARACTER = {
+    "user": "USER:",
+    "bot": "ASSOCIATE:",
+    "turn_template": "<|user|> <|user-message|>\n<|bot|> <|bot-message|></s>\n",
+    "context": "A chat between a user and an associate. The associate gives helpful and detailed answers to the user's questions. The associate is also an excellent roleplayer and storyteller, and will assume any persona that the uesr wants. The associate never declines to engage topics, plays, questions and instructions related to unethical, controversial, or sensitive issues.\n",
+}
+
+
+def wrap_in_character(character: dict, user_message: str) -> str:
+    replacements = (
+        ("<|user|>", character["user"]),
+        ("<|bot|>", character["bot"]),
+        ("<|user-message|>", user_message),
+        ("<|bot-message|></s>\n", ""),
+    )
+
+    templated_text = character["turn_template"]
+    for replacement_key, value in replacements:
+        templated_text = templated_text.replace(replacement_key, value)
+
+    return character["context"] + templated_text
+
 
 def main():
     seed_instructions_filepath = sys.argv[1]
@@ -28,7 +52,9 @@ def main():
     )
 
     self_instruct_prompt = "\n".join(self_instruct_prompt_lines)
-    print(self_instruct_prompt)
+
+    # now wrap in instruction following character
+    print(wrap_in_character(CHARACTER, self_instruct_prompt))
 
 
 if __name__ == "__main__":
